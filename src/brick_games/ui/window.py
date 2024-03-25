c8f8417ca745491575d31342155e6e27058ce130
Eridani Alcantar <ealcantar@utexas.edu>
2024-03-25 15:10:30 -0500

    Removed hardcoded fps.

--- src/brick_games/ui/window.py
+++ src/brick_games/ui/window.py
@@ -1,6 +1,7 @@
 import pyray as pr  # type: ignore
 
 IMAGE_NAME = 'games-six-six-six.png'
+FPS = 60
 
 
 class Window:
@@ -11,8 +12,7 @@ class Window:
 
     def create_window(self) -> None:
         """Create window"""
-        # TODO: Remove hard coded window image and fps.
         window_icon = pr.load_image(IMAGE_NAME)
         pr.init_window(self.width, self.height, self.window_title)
-        pr.set_target_fps(60)
+        pr.set_target_fps(FPS)
         pr.set_window_icon(window_icon)
